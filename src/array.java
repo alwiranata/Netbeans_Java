0b401804cb1521e68581a5ad3169e1b7b18e6ca9
Aldo Wiranata <awiranata263@gmail.com>
2024-12-05 10:51:24 +0700

    add : Class Array

---
+++ src/array.java
@@ -0,0 +1,14 @@
+
+
+public class array {
+
+  
+    public static void main(String[] args) {
+      String[] Handphone =  {"Samsung", "Vivo", "Iphone"};
+      System.out.println(Handphone.length);
+      for (String h : Handphone){
+          System.out.println(h);
+      }
+    }
+    
+}
